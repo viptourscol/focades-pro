8396a3d7b0f362997d41f8f66fbba5f74dd2fe3e
viptourscol <toursvip0@gmail.com>
2026-08-13 16:19:34 -0500

    fix: remove Helmet from AdminGeneradorTokens to fix build

--- src/pages/AdminGeneradorTokens.jsx
+++ src/pages/AdminGeneradorTokens.jsx
@@ -1,16 +1,9 @@
-import { Helmet } from 'react-helmet-async'
 import AdminTokenGeneratorPanel from '../components/AdminTokenGeneratorPanel'
 
 export default function AdminGeneradorTokens() {
   return (
-    <>
-      <Helmet>
-        <title>Generador de Tokens - Admin FOCADES</title>
-      </Helmet>
-      
-      <div className="space-y-6">
-        <AdminTokenGeneratorPanel />
-      </div>
-    </>
+    <div className="space-y-6">
+      <AdminTokenGeneratorPanel />
+    </div>
   )
 }

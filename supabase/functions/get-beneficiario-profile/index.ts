e69d97bd770c5e8d663b61822cecf48637044852
viptourscol <toursvip0@gmail.com>
2026-08-19 14:16:55 -0500

    fix: agregar x-client-info y apikey a headers CORS permitidos

--- supabase/functions/get-beneficiario-profile/index.ts
+++ supabase/functions/get-beneficiario-profile/index.ts
@@ -22,7 +22,7 @@ Deno.serve(async (req) => {
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-client-info, apikey',
       },
     })
   }
